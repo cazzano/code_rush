0bf269c9c6a59ed524cac7e88d65865c8aab7bac
cazzano <cazzano.agent.47@gmail.com>
2025-01-31 12:57:27 +0500

    Important

--- stable/rust/src/version.rs
+++ stable/rust/src/version.rs
@@ -1,4 +1,4 @@
 pub fn display_version() {
     println!("Version: 0.1.0");
-    println!("Link: http://www.example.com"); // Replace with your actual link
+    println!("Link: http://www.github.com/cazzano"); // Replace with your actual link
 }
